21ca96553fbdd6b1993aff854a67bebc0c8fe016
Carlos Monteiro <carlos_monteiro@poli.ufrj.br>
2018-05-25 21:04:43 -0300

    Add project files.

---
+++ PaymentGateway.CommandLine/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PaymentGateway.CommandLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
