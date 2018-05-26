2329b0a41188378b051b5f9d73ce51663fb5fa4a
Carlos Monteiro <carlos_monteiro@poli.ufrj.br>
2018-05-25 23:12:17 -0300

    Initial creation of the project's scructure.

--- PaymentGateway.CommandLine/Program.cs
+++ PaymentGateway.CommandLine/Program.cs
@@ -8,5 +8,7 @@ namespace PaymentGateway.CommandLine
         {
             Console.WriteLine("Hello World!");
         }
-    }
-}
+
+
+    } //class
+} //namespace
---
+++ PaymentGateway.Model/Entity/CreditCardBrandEnum.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+/// <summary>
+/// Enum with the credit card brand options.
+/// </summary>
+namespace PaymentGateway.Model.Entity
+{
+    enum CreditCardBrandEnum
+    {
+        [Description("Elo")]
+        Elo         = 1,
+        Visa        = 2,
+        Hipercard   = 3,
+        Mastercard  = 4
+    }
+}
